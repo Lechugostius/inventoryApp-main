8acdf5aa73c8be84a3b2587b7274033eaa4f41b5
juliantb1 <jmurillo@tacobell.cr>
2026-05-08 16:55:39 -0600

    Prueba rama
    rama julian

--- app/routes/nueva_entrada.py
+++ app/routes/nueva_entrada.py
@@ -234,6 +234,9 @@ def nueva_entrada_multiple():
                 
                 item_id = item_result["ID"]
 
+
+                #-----------------------------------------------------------------
+
                 # Verificar si debe quitarse del checklist de compras pendientes
                 check_and_finish_pending_purchase(item_id)
 
